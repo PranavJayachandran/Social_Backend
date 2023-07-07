645a3c746db82aa8fa5836621e2246dee13e9bcc
Pranav Jayachandran <77000606+PranavJayachandran@users.noreply.github.com>
2023-07-07 07:14:49 +0000

    Remove community added

--- db/community.js
+++ db/community.js
@@ -3,77 +3,87 @@ const { supabase } = require("./db_client");
 const { mapUser } = require("./userData");
 
 async function addCommunity(cover_image, banner_image, description, name) {
-    try {
-        const { data, error } = await supabase
-            .from('community')
-            .insert([
-                {
-                    cover_image: cover_image,
-                    banner_image: banner_image,
-                    description: description,
-                    members: 0,
-                    name: name
-                }
-            ])
-            .select('id')
+  try {
+    const { data, error } = await supabase
+      .from("community")
+      .insert([
+        {
+          cover_image: cover_image,
+          banner_image: banner_image,
+          description: description,
+          members: 0,
+          name: name,
+        },
+      ])
+      .select("id");
 
-        if (error) throw error
-        return data[0].id
-    } catch (error) {
-        console.error(error)
-    }
+    if (error) throw error;
+    return data[0].id;
+  } catch (error) {
+    console.error(error);
+  }
 }
-
-
-
 
 async function getCommunities() {
-    try {
-        const { data, error } = await supabase
-            .from('community')
-            .select('*')
-            .order('updated_at', { ascending: false })
-            ;
-
-        if (error) throw error
-        return data
-    } catch (error) {
-        console.error(error)
-    }
+  try {
+    const { data, error } = await supabase
+      .from("community")
+      .select("*")
+      .order("updated_at", { ascending: false });
+    if (error) throw error;
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
 }
 async function addCommunityToUser(user_id, community_id) {
-    try {
-        const { data, error } = await supabase
-            .from('community_to_user')
-            .insert([
-                {
-                    user_id: user_id,
-                    community_id: community_id
-                }
-            ])
+  try {
+    const { data, error } = await supabase.from("community_to_user").insert([
+      {
+        user_id: user_id,
+        community_id: community_id,
+      },
+    ]);
 
-        if (error) throw error
-        return data
-    } catch (error) {
-        console.error(error)
-    }
+    if (error) throw error;
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+async function removeUserfromCommunity(user_id, community_id) {
+  try {
+    const { data, error } = await supabase
+      .from("community_to_user")
+      .delete()
+      .match({ user_id: user_id })
+      .match({ community_id: community_id });
+  } catch (error) {
+    console.error(error);
+  }
 }
 async function toggleMembersInCommunity(community_id, function_name) {
-    try {
-        const { data, error } = await supabase.rpc(function_name, {
-            table_name: 'community',
-            column_name: "members",
-            condition: `id = ${community_id}`,
-        });
+  try {
+    const { data, error } = await supabase.rpc(function_name, {
+      table_name: "community",
+      column_name: "members",
+      condition: `id = ${community_id}`,
+    });
 
-        if (error) {
-            throw error;
-        }
-
-        console.log('Column value successfully.', function_name);
-    } catch (error) {
-        console.error('Error calling function:', function_name, error);
+    if (error) {
+      throw error;
     }
+
+    console.log("Column value successfully.", function_name);
+  } catch (error) {
+    console.error("Error calling function:", function_name, error);
+  }
 }
 
-module.exports = { addCommunity, getCommunities, addCommunityToUser, toggleMembersInCommunity }
+module.exports = {
+  addCommunity,
+  getCommunities,
+  addCommunityToUser,
+  toggleMembersInCommunity,
+  removeUserfromCommunity,
+};
--- db/userData.js
+++ db/userData.js
@@ -1,36 +1,38 @@
 const { supabase } = require("./db_client");
 
 async function getUserData(id) {
-    try {
-        const { data, error } = await supabase
-            .from('table_name')
-            .select('* ,community_to_user(community_id)')
-            .eq('id', id)
+  try {
+    const { data, error } = await supabase
+      .from("table_name")
+      .select("* ,community_to_user(community_id,community(*))")
+      .eq("id", id);
 
-        if (error) throw error
-        return data[0]
-    } catch (error) {
-        console.error(error)
-    }
+    if (error) throw error;
+    return data[0];
+  } catch (error) {
+    console.error(error);
+  }
 }
 async function mapUser(posts) {
-    let mapped_posts = [];
-    const asyncTasks = posts.map(async (item) => {
-        try {
-            const { data, error } = await supabase
-                .from('table_name')
-                .select('name,user_image')
-                .eq('id', item.user_id)
-            if (error) throw error
-            mapped_posts.push({ ...item, "username": data[0].name, "user_image": data[0].user_image })
-
-        } catch (error) {
-            console.error(error)
-        }
-    })
-    await Promise.all(asyncTasks);
-    return mapped_posts;
+  let mapped_posts = [];
+  const asyncTasks = posts.map(async (item) => {
+    try {
+      const { data, error } = await supabase
+        .from("table_name")
+        .select("name,user_image")
+        .eq("id", item.user_id);
+      if (error) throw error;
+      mapped_posts.push({
+        ...item,
+        username: data[0].name,
+        user_image: data[0].user_image,
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  });
+  await Promise.all(asyncTasks);
+  return mapped_posts;
 }
 
-
-module.exports = { getUserData, mapUser }
+module.exports = { getUserData, mapUser };
