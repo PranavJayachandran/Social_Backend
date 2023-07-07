51e505ff7370d28c1a30938f449d95cf365c1e80
Pranav Jayachandran <77000606+PranavJayachandran@users.noreply.github.com>
2023-07-07 06:39:04 +0000

    Corrected posts

--- db/posts.js
+++ db/posts.js
@@ -1,117 +1,111 @@
 const { supabase } = require("./db_client");
 
 async function addPosts(content, user_id) {
-    try {
-        const { data, error } = await supabase
-            .from('posts')
-            .insert([
-                {
-                    content: content,
-                    user_id: user_id,
-                    likes: 0,
-                    dislikes: 0,
-                    comment_id: []
-                }
-            ])
-            .select('id')
+  try {
+    const { data, error } = await supabase
+      .from("posts")
+      .insert([
+        {
+          content: content,
+          user_id: user_id,
+          likes: 0,
+          dislikes: 0,
+          comment_id: [],
+        },
+      ])
+      .select("id");
 
-        if (error) throw error
-        return data[0].id
-    } catch (error) {
-        console.error(error)
-    }
+    if (error) throw error;
+    return data[0].id;
+  } catch (error) {
+    console.error(error);
+  }
 }
-
-
-
 
 async function getPosts() {
-    let posts;
-    try {
-        const { data, error } = await supabase
-            .from('q')
-            .select('*')
+  let posts;
+  try {
+    const { data, error } = await supabase
+      .from("posts")
+      .select(
+        "*, comment(id,comment_content,upvote,downvote,user_id, table_name(name,user_image),upvotes_downvotes(id,user_id,value)),table_name(name,user_image),likes_dislikes(id,user_id,value)"
+      )
+      .order("updated_at", { ascending: false });
 
-        if (error) throw error
-        return data
-    } catch (error) {
-        console.error("Errorr", error)
-    }
+    if (error) throw error;
+    return data;
+  } catch (error) {
+    console.error("Errorr", error);
+  }
 }
 async function toggleLikeDislikeInPost(post_id, value, prev) {
-    let function_name = "";
-    let column_name1 = "";
-    let column_name2 = "";
-    if (value == 1 && prev == 0) {
-        function_name = "increment_column_value";
-        column_name1 = "likes";
+  let function_name = "";
+  let column_name1 = "";
+  let column_name2 = "";
+  if (value == 1 && prev == 0) {
+    function_name = "increment_column_value";
+    column_name1 = "likes";
+  } else if (value == 1 && prev == -1) {
+    function_name = "update_columns";
+    column_name1 = "likes";
+    column_name2 = "dislikes";
+  } else if (value == -1 && prev == 0) {
+    function_name = "increment_column_value";
+    column_name1 = "dislikes";
+  } else if (value == -1 && prev == 1) {
+    function_name = "update_columns";
+    column_name1 = "dislikes";
+    column_name2 = "likes";
+  } else if (value == 0) {
+    function_name = "decrement_column_value";
+    if (prev == 1) column_name1 = "likes";
+    else column_name1 = "dislikes";
+  } else if (prev == -2) {
+    if (value == 1) {
+      function_name = "increment_column_value";
+      column_name1 = "likes";
+    } else {
+      function_name = "increment_column_value";
+      column_name1 = "dislikes";
     }
-    else if (value == 1 && prev == -1) {
-        function_name = "update_columns";
-        column_name1 = "likes";
-        column_name2 = "dislikes";
-    }
-    else if (value == -1 && prev == 0) {
-        function_name = "increment_column_value";
-        column_name1 = "dislikes";
-    }
-    else if (value == -1 && prev == 1) {
-        function_name = "update_columns";
-        column_name1 = "dislikes";
-        column_name2 = "likes";
-    }
-    else if (value == 0) {
-        function_name = "decrement_column_value";
-        if (prev == 1)
-            column_name1 = "likes";
-        else
-            column_name1 = "dislikes";
-    }
-    else if (prev == -2) {
-        if (value == 1) {
-            function_name = "increment_column_value";
-            column_name1 = "likes"
-        }
-        else {
-            function_name = "increment_column_value";
-            column_name1 = "dislikes";
-        }
-    }
-    if (function_name == "update_columns") {
-        try {
-            const { data, error } = await supabase.rpc(function_name, {
-                table_name: 'posts',
-                column1_name: column_name1,
-                column2_name: column_name2,
-                condition: `id = ${post_id}`,
-            });
+  }
+  if (function_name == "update_columns") {
+    try {
+      const { data, error } = await supabase.rpc(function_name, {
+        table_name: "posts",
+        column1_name: column_name1,
+        column2_name: column_name2,
+        condition: `id = ${post_id}`,
+      });
 
-            if (error) {
-                throw error;
-            }
+      if (error) {
+        throw error;
+      }
 
-            console.log('Column value updated successfully.');
-        } catch (error) {
-            console.error('Error calling increment_column_value function:', error);
-        }
+      console.log("Column value updated successfully.");
+    } catch (error) {
+      console.error("Error calling increment_column_value function:", error);
     }
-    else if (function_name == "increment_column_value" || function_name == "decrement_column_value") {
-        try {
-            const { data, error } = await supabase.rpc(function_name, {
-                table_name: 'posts',
-                column_name: column_name1,
-                condition: `id = ${post_id}`,
-            });
+  } else if (
+    function_name == "increment_column_value" ||
+    function_name == "decrement_column_value"
+  ) {
+    try {
+      const { data, error } = await supabase.rpc(function_name, {
+        table_name: "posts",
+        column_name: column_name1,
+        condition: `id = ${post_id}`,
+      });
 
-            if (error) {
-                throw error;
-            }
+      if (error) {
+        throw error;
+      }
 
-            console.log('Column value incremented successfully.');
-        } catch (error) {
-            console.error('Error calling function:', function_name, error);
-        }
+      console.log("Column value incremented successfully.");
+    } catch (error) {
+      console.error("Error calling function:", function_name, error);
     }
+  }
 }
 
-module.exports = { addPosts, getPosts, toggleLikeDislikeInPost }
+module.exports = { addPosts, getPosts, toggleLikeDislikeInPost };
